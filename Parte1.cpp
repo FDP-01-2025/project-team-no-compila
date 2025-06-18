f280e638be92d3d1d38b40d1c814e70384bc20bc
Alvarez-Morales13 <00034725@uca.edu.sv>
2025-06-18 09:47:34 -0600

    parte1

---
+++ Parte1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(){
+    //Variable declaration
+    int option;
+    char a,b,c;
+
+
+
+    //Menu control
+    cout << "-----Menu-----" << endl;
+    cout << "1. Profile" << endl;
+    cout << "2. New game" << endl;
+    cout << "3. Saved game" << endl;
+    cout << "4. Salir" << endl;
+    cout << "5. Creditos" << endl;
+    cin >> option;
+
+
+
+    return 0;
+}
