4a69c7824e2d87c9aae5cc8fad5c302b619949c3
Erick José Claros López <00071125@uca.edu.sv>
2025-07-01 15:22:26 -0600

    def.cpp
    Carga el progreso de partida previamente guardada

--- src/def.cpp
+++ src/def.cpp
@@ -25,3 +25,38 @@ void progress(gameData player)
         cout << "The file could not be opened for reading." << endl;
     }
 }
+
+// Loads the previously saved game progress
+gameData loadProgress()
+{
+    gameData player;
+    ifstream read(file);
+    if (read.is_open())
+
+    {
+        read >> player.phase >> player.event >> player.pass;
+        for (int i = 0; i < 12; i++)
+        {
+            read >> player.inventory[i];
+        }
+
+        read.ignore();
+
+        getline(read, player.nameGame);
+        getline(read, player.player1);
+        getline(read, player.player2);
+        read.close();
+
+        cout << "\nProgress loaded: Phase " << player.phase << "\n\n";
+    }
+
+    else
+
+    {
+        cout << "Could not load the game." << endl;
+
+        player.phase = 1;
+    }
+    return player;
+}
+
